a0af05ee617289592fc5ff72848a5e8c45544ee3
venexene <venexene@gmail.com>
2025-09-23 22:07:16 +0300

    feat: Add task 10

--- L1_10/task10.go
+++ L1_10/task10.go
@@ -1,1 +1,17 @@
 package main
+
+import "fmt"
+
+func main() {
+	temps := [8]float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	slicedTemps := make(map[int][]float32) // Создание map срезов для результатов
+
+	for _, temp := range temps {
+		group := int(temp/10) * 10 // Вычисление группы 
+		slicedTemps[group] = append(slicedTemps[group], temp) // Добавление в срез по групппе
+	}
+
+	for key, value := range slicedTemps {
+		fmt.Printf("%d: %v\n", key, value)
+	}
+}
