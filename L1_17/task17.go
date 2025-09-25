ca055e3cf65a4b3698b462a770814c0f49232fcf
venexene <venexene@gmail.com>
2025-09-25 22:14:46 +0300

    feat: Add task 17

---
+++ L1_17/task17.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func binarySearch(arr []int, target int, first int, last int) int {
+	if last < first {
+		return -1
+	}
+
+	middle := first + (last - first) / 2
+
+	if arr[middle] == target {
+		return middle
+	} else if arr[middle] < target {
+		return binarySearch(arr, target, middle + 1, last)
+	} else {
+		return binarySearch(arr, target, first, middle - 1)
+	}
+}
+
+func main() {
+	arr := []int{1, 3, 4, 5, 6, 7, 10, 14}
+	target := 10
+
+	pos := binarySearch(arr, target, 0, len(arr) - 1)
+	fmt.Printf("Position of target: %d\n", pos)
+}
