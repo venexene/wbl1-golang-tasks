88e65793a95d7bcf6323ec54ef9c7a8b50675715
venexene <venexene@gmail.com>
2025-10-02 21:46:59 +0300

    feat: Add task 22

--- L1_22/task22.go
+++ L1_22/task22.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	a := new(big.Int)
+	a.SetString("1000000000000000000000", 10)
+	b := new(big.Int)
+	b.SetString("200000000000000000000", 10)
+
+	sum := new(big.Int).Add(a, b)
+	diff := new(big.Int).Sub(a, b)
+	prod := new(big.Int).Mul(a, b)
+	quot := new(big.Int).Quo(a, b)
+	
+	fmt.Printf("a = %s\n", a.String())
+	fmt.Printf("b = %s\n", b.String())
+	fmt.Printf("a + b = %s\n", sum.String())
+	fmt.Printf("a - b = %s\n", diff.String())
+	fmt.Printf("a * b = %s\n", prod.String())
+	fmt.Printf("a / b = %s\n", quot.String())
+}
