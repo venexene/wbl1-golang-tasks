4fa06d9d9fd4ed161c9034f8d63422edae60b461
venexene <venexene@gmail.com>
2025-10-02 22:04:18 +0300

    feat: Add task 23

--- L1_23/task23.go
+++ L1_23/task23.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func deleteFromSlice(slice []int, ind int) []int {
+	copy(slice[ind:], slice[ind+1:]) // Сдвиг элементов влево
+	slice = slice[:len(slice)-1] // Уменьшение размера слайса
+	return slice
+}
+
+func main() {
+	slice := []int{0, 1, 2, 3, 4, 5}
+	slice = deleteFromSlice(slice, 2)
+	fmt.Println(slice)
+}
