e6f9b30619f70bdc60f08f0973df54f7ab9e8a26
venexene <venexene@gmail.com>
2025-10-03 11:47:44 +0300

    feat: Add task 26

--- L1_26/task26.go
+++ L1_26/task26.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func checkUniqueness(str string) bool {
+	str = strings.ToLower(str)
+	set := make(map[rune]bool)
+
+	for _, char := range str {
+		if set[char] {
+			return false
+		}
+		set[char] = true
+	}
+
+	return true
+}
+
+func main() {
+	str := "abcd"
+	fmt.Printf("\"%s\" -> %t\n", str,  checkUniqueness(str))
+
+	str = "abCdefAaf"
+	fmt.Printf("\"%s\" -> %t\n", str, checkUniqueness(str))
+
+	str = "aabcd"
+	fmt.Printf("\"%s\" -> %t\n", str, checkUniqueness(str))
+}
